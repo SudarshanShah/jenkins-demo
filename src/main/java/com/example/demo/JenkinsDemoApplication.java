fe1024ccd19c98fa91735c7885aebc710bcc7be5
Sudarshan Shah <sudarshanshah911@gmail.com>
2022-04-18 18:05:17 +0530

    first commit

---
+++ src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void initFunc() {
+		logger.info("Application started");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info("Test case running...");
+		Assertions.assertEquals(true, true);
+	}
+
+}
